1c19a23cfea39589def52dc41e8eb8de245385ff
朱文哲 <jtrkid@gmail.com>
2018-01-24 10:29:01 +0800

    Make isf read parameter from ini file

--- isf.py
+++ isf.py
@@ -5,14 +5,27 @@ from __future__ import print_function
 import argparse
 import logging.handlers
 import os
-
+import ConfigParser
 from icssploit.interpreter import IcssploitInterpreter
 
-log_handler = logging.handlers.RotatingFileHandler(filename='icssploit.log', maxBytes=500000)
+# Define conf
+isf_conf_file = "isf.ini"
+isf_conf = ConfigParser.SafeConfigParser(allow_no_value=True)
+isf_conf.read(isf_conf_file)
+
+# Get parameter from conf
+log_file_name = isf_conf.get("LOG", "log_file_name")
+log_max_bytes = isf_conf.getint("LOG", "log_max_bytes")
+log_level = isf_conf.getint("LOG", "log_level")
+package_path = isf_conf.get("EXTRA_PACKEAGE", "package_path")
+
+
+# Define logger
+log_handler = logging.handlers.RotatingFileHandler(filename=log_file_name, maxBytes=log_max_bytes)
 log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s       %(message)s')
 log_handler.setFormatter(log_formatter)
 LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.DEBUG)
+LOGGER.setLevel(log_level)
 LOGGER.addHandler(log_handler)
 
 parser = argparse.ArgumentParser(description='ICSSploit - ICS Exploitation Framework')
@@ -22,14 +35,16 @@ parser.add_argument('-e',
                     help='Add extra packet(clients, modules, protocols) to isf.')
 
 
-def icssploit(extra_package_path=None):
+def icssploit(extra_package_path=package_path):
+    if not os.path.isdir(extra_package_path):
+        extra_package_path = None
     isf = IcssploitInterpreter(extra_package_path)
     isf.start()
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
     if args.extra_package_path:
-        if os.path.isdir(args.extra_package_path):
             icssploit(extra_package_path=args.extra_package_path)
     else:
         icssploit()
